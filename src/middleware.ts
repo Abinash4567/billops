30109c68d9c7b07814c8d04835aa776f79350e0e
Abinash4567 <abinashyadav4567@gmail.com>
2024-04-15 12:21:18 +0530

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
 
     const isPublicPath = path === '/login' || path === '/register' || path==='/dashboard/docs';
 
-    const token = request.cookies.get('next-auth.session-token')?.value || ''
+    const token = request.cookies.get('__Secure-next-auth.session-token')?.value || ''
 
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl))
