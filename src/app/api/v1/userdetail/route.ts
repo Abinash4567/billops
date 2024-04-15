44e924ff7d52060b3120c9ef57eff5a262a15dbb
Abinash Ray Yadav <98229006+Abinash4567@users.noreply.github.com>
2024-04-15 19:51:31 +0530

    Update route.ts

--- src/app/api/v1/userdetail/route.ts
+++ src/app/api/v1/userdetail/route.ts
@@ -5,14 +5,14 @@ export async function POST(req: Request, res: Response) {
         const { orgId, userId } = await req.json();
         console.log(userId);
         let key = req.headers.get("x-api-key");
-        // const orgKey = await db.orgModel.findUnique({
-        //     where: {
-        //         id: parseInt(orgId),
-        //     },
-        //     select: {
-        //         APIKey: true,
-        //     },
-        // });
+        const orgKey = await db.orgModel.findUnique({
+            where: {
+                id: parseInt(orgId),
+            },
+            select: {
+                APIKey: true,
+            },
+        });
 
         // if (!orgKey || orgKey.APIKey !== key || !userId) {
         //     return new Response(
@@ -26,38 +26,38 @@ export async function POST(req: Request, res: Response) {
         //     );
         // }
 
-        // const user = await db.userModel.findMany({
-        //     where: {
-        //         userId: userId,
-        //         plan: {
-        //             organization: {
-        //                 id: parseInt(orgId),
-        //             },
-        //         },
-        //     },
-        //     select: {
-        //         id: true,
-        //         name: true,
-        //         email: true,
-        //         planExpiry: true,
-        //         plan: {
-        //             select: { type: true },
-        //         },
-        //     },
-        // });
-        // let response = [];
-        // for (let item of user) {
-        //     let temp = {
-        //         id: item.id,
-        //         name: item.name,
-        //         email: item.email,
-        //         planExpiry: item.planExpiry,
-        //         planType: item.plan.type
-        //     };
-        //     response.push(temp);
-        // }
+        const user = await db.userModel.findMany({
+            where: {
+                userId: userId,
+                plan: {
+                    organization: {
+                        id: parseInt(orgId),
+                    },
+                },
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                planExpiry: true,
+                plan: {
+                    select: { type: true },
+                },
+            },
+        });
+        let response = [];
+        for (let item of user) {
+            let temp = {
+                id: item.id,
+                name: item.name,
+                email: item.email,
+                planExpiry: item.planExpiry,
+                planType: item.plan.type
+            };
+            response.push(temp);
+        }
 
-        return new Response(JSON.stringify({ user: userId, orgId: orgId, key: key }), {
+        return new Response(JSON.stringify({ user: userId, orgId: orgId, key: key, orgKey: orgKey, user: user, response: response }), {
             status: 200,
             headers: {
                 "Content-Type": "application/json",
